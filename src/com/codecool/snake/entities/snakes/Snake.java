c16670734056ce79c735de93e5f2c8bc8d367461
garyAdam <rapture047@gmail.com>
2019-03-27 10:49:27 +0100

    Merge branch 'development' of /home/gary/IdeaProjects/java-snake-game-aszentlelek with conflicts.

--- src/com/codecool/snake/entities/snakes/Snake.java
+++ src/com/codecool/snake/entities/snakes/Snake.java
@@ -7,12 +7,17 @@ import com.codecool.snake.entities.GameEntity;
 import com.codecool.snake.eventhandler.InputHandler;
 
 import com.sun.javafx.geom.Vec2d;
+import javafx.scene.effect.*;
 import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 
 public class Snake implements Animatable {
     private static final float speed = 2;
     private int health = 100;
+
 
     private SnakeHead head;
     private DelayedModificationList<GameEntity> body;
@@ -24,6 +29,11 @@ public class Snake implements Animatable {
 
         addPart(4);
     }
+
+    public int getHealth() {
+        return health;
+    }
+
 
     public void step() {
         SnakeControl turnDir = getUserInput();
@@ -60,8 +70,64 @@ public class Snake implements Animatable {
     private void checkForGameOverConditions() {
         if (head.isOutOfBounds() || health <= 0) {
             System.out.println("Game Over");
+            Globals.getInstance().display.clear();
+            Globals.getInstance().game.getChildren().add(gameOverText("Game Over",80,Globals.WINDOW_WIDTH/2.5,Globals.WINDOW_HEIGHT/3));
+            Globals.getInstance().game.getChildren().add(gameOverText("Lorem ipsssuuummm Lorem ipsssuuummm Lorem ipsssuuummm Lorem ipsssuuummm Lorem ipsssuuummm Lorem ipsssuuummm Lorem ipsssuuummm Lorem ipsssuuummm Lorem ipsssuuummm Lorem ipsssuuummm Lorem ipsssuuummm Lorem ipsssuuummm Lorem ipsssuuummm Lorem ipsssuuummm Lorem ipsssuuummm ",40,Globals.WINDOW_WIDTH/3.5,Globals.WINDOW_HEIGHT/2));
             Globals.getInstance().stopGame();
         }
+    }
+
+    private Text gameOverText(String txt, int size, double x, double y){
+        Text text = new Text();
+        text.setX(x);
+        text.setY(y);
+        text.setText(txt);
+        text.setFont(Font.font ("Alex Brush", size));
+        text.setFill(Color.CRIMSON);
+        text.setCache(true);
+
+        Blend blend = new Blend();
+        blend.setMode(BlendMode.MULTIPLY);
+
+        DropShadow ds = new DropShadow();
+        ds.setColor(Color.rgb(254, 235, 66, 0.3));
+        ds.setOffsetX(5);
+        ds.setOffsetY(5);
+        ds.setRadius(5);
+        ds.setSpread(0.2);
+
+        blend.setBottomInput(ds);
+
+        DropShadow ds1 = new DropShadow();
+        ds1.setColor(Color.web("#f13a00"));
+        ds1.setRadius(20);
+        ds1.setSpread(0.2);
+
+        Blend blend2 = new Blend();
+        blend2.setMode(BlendMode.MULTIPLY);
+
+        InnerShadow is = new InnerShadow();
+        is.setColor(Color.web("#feeb42"));
+        is.setRadius(9);
+        is.setChoke(0.8);
+        blend2.setBottomInput(is);
+
+        InnerShadow is1 = new InnerShadow();
+        is1.setColor(Color.web("#f13a00"));
+        is1.setRadius(5);
+        is1.setChoke(0.4);
+        blend2.setTopInput(is1);
+
+        Blend blend1 = new Blend();
+        blend1.setMode(BlendMode.MULTIPLY);
+        blend1.setBottomInput(ds1);
+        blend1.setTopInput(blend2);
+
+        blend.setTopInput(blend1);
+
+        text.setEffect(blend);
+
+        return text;
     }
 
     private void updateSnakeBodyHistory() {
