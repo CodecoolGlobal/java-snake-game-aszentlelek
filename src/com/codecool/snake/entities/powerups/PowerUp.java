35256b250743190b62a7bb97fd6f19ff8effc6bb
Sütő Bence 'Owen <ssben636@gmail.com>
2019-03-27 11:13:45 +0100

    powerup heals

--- src/com/codecool/snake/entities/powerups/PowerUp.java
+++ src/com/codecool/snake/entities/powerups/PowerUp.java
@@ -1,12 +1,12 @@
-package com.codecool.snake.entities.enemies;
+package com.codecool.snake.entities.powerups;
 
 import com.codecool.snake.Globals;
 import com.codecool.snake.entities.GameEntity;
 
-public abstract class Enemy extends GameEntity{
+public abstract class PowerUp extends GameEntity{
     private int damage;
 
-    Enemy(int damage) {
+    PowerUp(int damage) {
         double distanceFromSnakeHead;
         do {
             generateRandomXY();
--- src/com/codecool/snake/entities/powerups/Satan.java
+++ src/com/codecool/snake/entities/powerups/Satan.java
@@ -4,13 +4,15 @@ import com.codecool.snake.entities.GameEntity;
 import com.codecool.snake.Globals;
 import com.codecool.snake.entities.Interactable;
 import com.codecool.snake.entities.snakes.SnakeHead;
+import com.codecool.snake.entities.powerups.PowerUp;
 import java.util.Random;
 
 
-public class Satan extends GameEntity implements Interactable {
+public class Satan extends PowerUp implements Interactable {
     private static Random rnd = new Random();
 
     public Satan() {
+        super(10);
         setImage(Globals.getInstance().getImage("Satan"));
 
         setX(rnd.nextDouble() * Globals.WINDOW_WIDTH);
